12585ce47335bc4e54626ea457b8933660e30e56
tyesc <tyler@poool.fr>
2026-05-22 17:48:31 +0200

    feat: can listen input from midi device

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod utils;
+
 fn main() {
-    sound_mission_lib::run()
+    match utils::listen_inputs() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+
+    // sound_mission_lib::run();
 }
