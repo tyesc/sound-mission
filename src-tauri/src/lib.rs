22b21f3766c55394679606b2e7fba6ce87404d6b
tyesc <escolano.tyler@gmail.com>
2026-05-22 23:33:17 +0200

    refactor: clean code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,6 @@
 use serde::Serialize;
-
 use midir::{Ignore, MidiInput};
 
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
